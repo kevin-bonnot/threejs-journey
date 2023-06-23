d7e4246a2db56734c84607f459e734b8589f21bf
Kévin Bonnot <bonnot.kevin@gmail.com>
2023-06-23 21:06:22 +0200

    blender

--- Advanced/22-imported-models/src/script.js
+++ Advanced/22-imported-models/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import * as dat from 'lil-gui'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader'
 
 /**
  * Base
@@ -15,18 +17,46 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Models
+ */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer
+
+gltfLoader.load(
+  '/models/Fox/glTF/Fox.gltf',
+  (gltf) => {
+    // const children = [...gltf.scene.children]
+    // for(const child of children) {
+    //     scene.add(child)
+    // }
+
+    mixer = new THREE.AnimationMixer(gltf.scene)
+    const action = mixer.clipAction(gltf.animations[2])
+    action.play()
+
+    gltf.scene.scale.set(.025, .025, .025)
+    scene.add(gltf.scene)
+  },
+)
+
+/**
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: '#444444',
+    metalness: 0,
+    roughness: 0.5
+  })
 )
 floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
+floor.rotation.x = -Math.PI * 0.5
 scene.add(floor)
 
 /**
@@ -39,10 +69,10 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.left = -7
 directionalLight.shadow.camera.top = 7
 directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
+directionalLight.shadow.camera.bottom = -7
 directionalLight.position.set(5, 5, 5)
 scene.add(directionalLight)
 
@@ -50,23 +80,22 @@ scene.add(directionalLight)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -86,7 +115,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
@@ -99,20 +128,24 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 let previousTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
-    // Update controls
-    controls.update()
+  // Update mixer
+  if (!!mixer) {
+    mixer.update(deltaTime)
+  }
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update()
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
--- Classics/20-raycaster/src/script.js
+++ Classics/20-raycaster/src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 
 /**
@@ -19,19 +19,19 @@ const scene = new THREE.Scene()
  * Objects
  */
 const object1 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({color: '#ff0000'})
 )
-object1.position.x = - 2
+object1.position.x = -2
 
 const object2 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({color: '#ff0000'})
 )
 
 const object3 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({color: '#ff0000'})
 )
 object3.position.x = 2
 
@@ -58,23 +58,22 @@ const raycaster = new THREE.Raycaster()
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -83,24 +82,24 @@ window.addEventListener('resize', () =>
 const mouse = new THREE.Vector2()
 
 window.addEventListener('mousemove', (_event) => {
-    mouse.x = (_event.clientX / sizes.width) * 2 - 1
-    mouse.y = - (_event.clientY / sizes.height) * 2 + 1
+  mouse.x = (_event.clientX / sizes.width) * 2 - 1
+  mouse.y = -(_event.clientY / sizes.height) * 2 + 1
 })
 
 window.addEventListener('click', (_event) => {
-    if (!!currentIntersect) {
-        switch (currentIntersect.object) {
-            case object1 :
-                console.log('click on a sphere 1')
-                break
-            case object2 :
-                console.log('click on a sphere 2')
-                break
-            case object3 :
-                console.log('click on a sphere 3')
-                break
-        }
+  if (!!currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1 :
+        console.log('click on a sphere 1')
+        break
+      case object2 :
+        console.log('click on a sphere 2')
+        break
+      case object3 :
+        console.log('click on a sphere 3')
+        break
     }
+  }
 })
 
 /**
@@ -119,7 +118,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -131,55 +130,54 @@ const clock = new THREE.Clock()
 
 let currentIntersect = null
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Animete
-    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
-    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
-    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+  // Animete
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
 
-    // Cast a ray
-    raycaster.setFromCamera(mouse, camera)
+  // Cast a ray
+  raycaster.setFromCamera(mouse, camera)
 
-    // const rayOrigin = new THREE.Vector3(-3, 0, 0)
-    // const rayDirection = new THREE.Vector3(1, 0, 0)
+  // const rayOrigin = new THREE.Vector3(-3, 0, 0)
+  // const rayDirection = new THREE.Vector3(1, 0, 0)
 
-    // raycaster.set(rayOrigin, rayDirection)
+  // raycaster.set(rayOrigin, rayDirection)
 
-    const objectsToTest = [object1, object2, object3]
-    const intersects = raycaster.intersectObjects(objectsToTest)
+  const objectsToTest = [object1, object2, object3]
+  const intersects = raycaster.intersectObjects(objectsToTest)
 
-    objectsToTest.forEach(obj => {
-        obj.material.color.set('#f00')
-    })
+  objectsToTest.forEach(obj => {
+    obj.material.color.set('#f00')
+  })
 
-    intersects.forEach(intersect => {
-        intersect.object.material.color.set('#00ff00')
-    })
+  intersects.forEach(intersect => {
+    intersect.object.material.color.set('#00ff00')
+  })
 
-    if (intersects.length > 0) {
-        if (currentIntersect == null) {
-            console.log('mouse enter')
-        }
-        currentIntersect = intersects[0]
-    } else {
-        if (currentIntersect) {
-            console.log('mouse leave')
-        }
-        currentIntersect = null
+  if (intersects.length > 0) {
+    if (currentIntersect == null) {
+      console.log('mouse enter')
     }
+    currentIntersect = intersects[0]
+  } else {
+    if (currentIntersect) {
+      console.log('mouse leave')
+    }
+    currentIntersect = null
+  }
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
