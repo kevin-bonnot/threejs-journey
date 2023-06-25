ff04936ec17596b5b0e68eb8e4356256823810f9
Kévin Bonnot <bonnot.kevin@gmail.com>
2023-06-26 01:01:23 +0200

    end of lesson 46

---
+++ React/46-environment-and-staging-with-r3f/src/Experience.jsx
@@ -0,0 +1,177 @@
+import {useFrame} from '@react-three/fiber'
+import {ContactShadows, Environment, Lightformer, OrbitControls, Sky, Stage, useHelper} from '@react-three/drei'
+import {useRef} from 'react'
+import {Perf} from 'r3f-perf'
+import {DirectionalLightHelper} from "three";
+import {useControls} from "leva";
+
+export default function Experience() {
+  const cube = useRef()
+  const directionalLight = useRef()
+
+  useHelper(directionalLight, DirectionalLightHelper, 1)
+
+  useFrame((state, delta) => {
+    // const time = state.clock.elapsedTime;
+
+    cube.current.rotation.y += delta * 0.2
+    // cube.current.position.x = 2 + Math.sin(time)
+  })
+
+  const {color, opacity, blur} = useControls('shadows', {
+    color: '#4b2709',
+    opacity: {
+      value: 0.4,
+      min: 0,
+      max: 1,
+      step: 0.01
+    },
+    blur: {
+      value: 2.8,
+      min: 0,
+      max: 10,
+      step: 0.01
+    },
+  })
+
+  const {sunPosition} = useControls('Sky', {
+    sunPosition: {
+      value: [1, 2, 3]
+    }
+  })
+
+  const {envMapIntensity, envMapHeight, envMapRadius, envMapScale} = useControls('environment maps', {
+    envMapIntensity: {value: 3.5, min: 0, max: 12},
+    envMapHeight: {value: 7, min: 0, max: 100},
+    envMapRadius: {value: 20, min: 10, max: 1000},
+    envMapScale: {value: 100, min: 10, max: 1000},
+  })
+
+  return <>
+
+    <Environment
+      // files="./environmentMaps/venice_sunset_2k.hdr"
+      preset="apartment"
+      // resolution={32}
+      ground={{
+        height: envMapHeight,
+        radius: envMapRadius,
+        scale: envMapScale
+      }}
+    >
+      <color args={['black']} attach="background"/>
+      <Lightformer
+        position-z={-5}
+        scale={10}
+        color={'red'}
+        intensity={10}
+        form="ring"
+      />
+      <mesh position-z={-5} scale={10}>
+        <planeGeometry/>
+        <meshBasicMaterial color={[10, 0, 0]}/>
+      </mesh>
+    </Environment>
+
+    {/*<BakeShadows />*/}
+    {/*<SoftShadows*/}
+    {/*  frustum={3.75}*/}
+    {/*  size={50}*/}
+    {/*  near={9.5}*/}
+    {/*  samples={17}*/}
+    {/*  rings={11}*/}
+    {/*/>*/}
+
+
+    <Perf position="top-left"/>
+
+    <OrbitControls makeDefault/>
+
+    {/*<AccumulativeShadows*/}
+    {/*  position={[0, -0.999, 0]}*/}
+    {/*  scale={10}*/}
+    {/*  color={'#316d39'}*/}
+    {/*  opacity={0.8}*/}
+    {/*  frames={Infinity}*/}
+    {/*  temporal*/}
+    {/*  blend={100}*/}
+    {/*>*/}
+    {/*  <RandomizedLight*/}
+    {/*    position={[1, 2, 3]}*/}
+    {/*    amount={8}*/}
+    {/*    radius={1}*/}
+    {/*    ambient={0.5}*/}
+    {/*    intensity={1}*/}
+    {/*    bias={0.001}*/}
+    {/*  />*/}
+    {/*</AccumulativeShadows>*/}
+
+    {/*<ContactShadows*/}
+    {/*  position={[0, -0, 0]}*/}
+    {/*  scale={10}*/}
+    {/*  resolution={512}*/}
+    {/*  far={5}*/}
+    {/*  blur={blur}*/}
+    {/*  opacity={opacity}*/}
+    {/*  color={color}*/}
+    {/*  frames={1}*/}
+    {/*/>*/}
+
+    {/*<directionalLight*/}
+    {/*  ref={directionalLight}*/}
+    {/*  position={sunPosition}*/}
+    {/*  intensity={1.5}*/}
+    {/*  castShadow*/}
+    {/*  shadow-mapSize={[1024, 1024]}*/}
+    {/*  shadow-camera-top={5}*/}
+    {/*  shadow-camera-right={5}*/}
+    {/*  shadow-camera-bottom={-5}*/}
+    {/*  shadow-camera-left={-5}*/}
+    {/*  shadow-camera-near={1}*/}
+    {/*  shadow-camera-far={10}*/}
+    {/*/>*/}
+    {/*<ambientLight intensity={0.5}/>*/}
+
+    <Sky
+      sunPosition={sunPosition}
+    />
+
+    {/*<mesh castShadow position-y={1} position-x={-2}>*/}
+    {/*  <sphereGeometry/>*/}
+    {/*  <meshStandardMaterial color="orange" envMapIntensity={envMapIntensity}/>*/}
+    {/*</mesh>*/}
+
+    {/*<mesh castShadow position-y={1} ref={cube} position-x={2} scale={1.5}>*/}
+    {/*  <boxGeometry/>*/}
+    {/*  <meshStandardMaterial color="mediumpurple" envMapIntensity={envMapIntensity}/>*/}
+    {/*</mesh>*/}
+
+    {/*<Stage*/}
+    {/*  shadows={{*/}
+    {/*    type: 'contact',*/}
+    {/*    opacity: 0.2,*/}
+    {/*    blur: 3*/}
+    {/*  }}*/}
+    {/*  environment="sunset"*/}
+    {/*  preset="portrait"*/}
+    {/*  intensity={2}*/}
+    {/*>*/}
+      <mesh castShadow position-y={1} position-x={-2}>
+        <sphereGeometry/>
+        <meshStandardMaterial color="orange" envMapIntensity={envMapIntensity}/>
+      </mesh>
+
+      <mesh castShadow position-y={1} ref={cube} position-x={2} scale={1.5}>
+        <boxGeometry/>
+        <meshStandardMaterial color="mediumpurple" envMapIntensity={envMapIntensity}/>
+      </mesh>
+    {/*</Stage>*/}
+
+
+    {/*<mesh position-y={0} rotation-x={-Math.PI * 0.5} scale={10}>*/}
+    {/*  <planeGeometry/>*/}
+    {/*  <meshStandardMaterial color="greenyellow" envMapIntensity={envMapIntensity}/>*/}
+    {/*</mesh>*/}
+
+  </>
+}
---
+++ React/46-environment-and-staging-with-r3f/src/index.jsx
@@ -0,0 +1,25 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import {Canvas} from '@react-three/fiber'
+import Experience from './Experience.jsx'
+import {Leva} from "leva";
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+  <>
+    <Leva/>
+    <Canvas
+      shadows={false}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
+    >
+      <color args={ ['ivory'] } attach="background" />
+      <Experience/>
+    </Canvas>
+  </>
+)
