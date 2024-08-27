1254c67311a68dea9164b8d5d6e68fb3959c7eac
Kévin Bonnot <bonnot.kevin@gmail.com>
2024-08-27 16:23:27 +0200

    End of lesson 26 and setup lesson 27

--- Advanced/26-code-structuring/src/Experience/Experience.js
+++ Advanced/26-code-structuring/src/Experience/Experience.js
@@ -6,6 +6,7 @@ import Renderer from './Renderer'
 import World from './world/World'
 import Resources from './utils/Resources'
 import sources from './sources'
+import Debug from './utils/Debug'
 
 let instance = null
 
@@ -24,6 +25,7 @@ export default class Experience {
     this.canvas = canvas
 
     // Setup
+    this.debug = new Debug()
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
@@ -49,6 +51,34 @@ export default class Experience {
 
   update() {
     this.camera.update()
+    this.world.update()
     this.renderer.update()
+  }
+
+  destroy() {
+    this.sizes.off('resize')
+    this.time.off('tick')
+
+    // Traverse the scene
+    this.scene.traverse(child => {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.dispose()
+
+        for (const key in child.material) {
+          const value = child.material[key]
+
+          if (value && typeof value.dispose === 'function') {
+            value.dispose()
+          }
+        }
+      }
+    })
+
+    this.camera.controls.dispose()
+    this.renderer.instance.dispose()
+
+    if (this.debug.active) {
+      this.debug.ui.destroy()
+    }
   }
 }
--- Advanced/26-code-structuring/src/Experience/world/World.js
+++ Advanced/26-code-structuring/src/Experience/world/World.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import Experience from "../Experience";
 import Environment from './Environment';
+import Floor from './Floor';
+import Fox from './Fox';
 
 export default class World {
   constructor() {
@@ -8,16 +10,17 @@ export default class World {
     this.scene = this.experience.scene
     this.resources = this.experience.resources
 
-    // Text Mesh
-    const testMesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshStandardMaterial()
-    )
-    this.scene.add(testMesh)
-
     this.resources.on('ready', () => {
       // Setup
+      this.floor = new Floor()
+      this.fox = new Fox()
       this.environment = new Environment()
     })
+  }
+
+  update() {
+    if (this.fox) {
+      this.fox.update()
+    }
   }
 }
